0833ea62dc972bc6cc1409c415827310e692b877
KuHavoc <dmitroivanyta@gmail.com>
2025-05-09 17:46:05 +0300

    Add mode chooses logic

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log("Hello world!");
+function main() {
+    const args = process.argv.slice(2);
+    if (args.length === 0) {
+        interactive().catch(err => {
+            console.error(err);
+            process.exit(1);
+        });
+    } else if (args.length === 1) {
+        nonInteractive(args[0]);
+    }
+}
+
+main();
+
+async function interactive() {
+    console.log('interactive');
+}
+
+function nonInteractive(args: string) {
+    console.log(`nonInteractive file path: ${args}`);
+}
