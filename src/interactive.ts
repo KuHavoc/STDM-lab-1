4e00071837d4daa4090e0946ae7feebc47c637b6
KuHavoc <dmitroivanyta@gmail.com>
2025-05-09 17:52:42 +0300

    Implement basic input logic for interactive mode

--- src/interactive.ts
+++ src/interactive.ts
@@ -1,5 +1,34 @@
-async function interactive() {
-    console.log('interactive');
+import readline from 'readline';
+
+async function interactive(): Promise<void> {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    const question = (prompt: string): Promise<string> => {
+        return new Promise(resolve => {
+            return new Promise(_ => {
+                rl.question(prompt, () => {});
+            });
+        });
+    };
+
+    const getCoefficient = async (name: string): Promise<number> => {
+        while (true) {
+            const answer = await question(`${name} = `);
+            const value = parseFloat(answer);
+            if (!isNaN(value) && answer.trim() !== '') {
+                return value;
+            } else {
+                console.log(`Error. Expected a valid real number, got ${answer} instead`);
+            }
+        }
+    };
+    getCoefficient('a');
+    getCoefficient('b');
+    getCoefficient('c');
+    rl.close();
 }
 
 export default interactive;
